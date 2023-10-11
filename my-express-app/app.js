36829fa5f7ce62a574e63fa14e88f53ba84be01d
gavinhowell <ghowell2@murraystate.edu>
2023-10-10 19:55:36 -0500

    react app working but database is not integrated
    yeah

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const app = express();
-const port = 3000; // You can choose any available port
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const taskRoutes = require('./routes/tasks');
 
-// Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, Express.js!');
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://Database-Access:TheRogueSlayer@csc425-project-database.obdl3wj.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+// Middleware setup
+app.use(bodyParser.json());
+app.use(cors());
+
+// Use task routes
+app.use('/api', taskRoutes);
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// Define an API route
-app.get('/api/data', (req, res) => {
-    const data = { message: 'This is an API endpoint.' };
-    res.json(data);
-  });
